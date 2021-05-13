579b244abd8d9686161b4dde155e4ca68ca0e18e
tpholston <tholston@ucsd.edu>
2021-05-13 10:02:08 -0700

    calendarDemo with sidebar

--- docs/misc/calendarDemo/scripts/script.js
+++ docs/misc/calendarDemo/scripts/script.js
@@ -67,3 +67,13 @@ eventCreation.addEventListener('submit', function () {
 
   createCalendar(existing);
 });
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
