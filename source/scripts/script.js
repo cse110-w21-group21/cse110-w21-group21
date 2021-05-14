863890c4e890def7c253d95bb393aade93b86e7f
lucasbajoua <lucasbajoua@gmail.com>
2021-05-14 14:41:27 -0700

    ci pull request attempt

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,11 +1,12 @@
+/* eslint-disable */
 document.getElementById("mySideBar").addEventListener("onclick", openNav);
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
