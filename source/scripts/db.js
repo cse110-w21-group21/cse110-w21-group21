d490fef11b6cc145b0022c61709b97cb771911f5
Thomas Garry <32282600+thomas-gg@users.noreply.github.com>
2021-05-15 22:10:06 -0700

    fixed some eslint errors

--- source/scripts/db.js
+++ source/scripts/db.js
@@ -1,26 +1,26 @@
 function createDB() {
-  const request = indexedDB.open("noteDB", 1);
+  const request = indexedDB.open('noteDB', 1);
 
   // on upgrade needed --> if database doesn't exist
   request.onupgradeneeded = (e) => {
-    db = e.target.result;
+    const db = e.target.result;
 
-    let objStore = db.createObjectStore("personal_notes", {
-      keyPath: "date",
+    const objStore = db.createObjectStore('personal_notes', {
+      keyPath: 'date',
     });
-    objStore.createIndex("date", "date", { unique: false });
+    objStore.createIndex('date', 'date', { unique: false });
     console.log(
-      `upgrade is called database name: ${db.name} version : ${db.version}`
+      `upgrade is called database name: ${db.name} version : ${db.version}`,
     );
   };
-  //on success
+  // on success
   request.onsuccess = (e) => {
-    db = e.target.result;
+    const db = e.target.result;
     console.log(
-      `success is called database name: ${db.name} version : ${db.version}`
+      `success is called database name: ${db.name} version : ${db.version}`,
     );
   };
-  //on error
+  // on error
   request.onerror = (e) => {
     console.log(`error: ${e.target.error} was found `);
   };
@@ -29,29 +29,29 @@ function createDB() {
 function addNote(event) {
   // allRecords.innerHTML = "";
   const today = new Date();
-  const date =
-    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-  const noteString = document.getElementById("noteList").innerHTML;
+  const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+  const noteString = document.getElementById('noteList').innerHTML;
   const note = {
     time: Math.floor(Date.now() / 1000),
-    date: date,
+    date,
     text: noteString,
   };
   console.log(note);
-  const tx = db.transaction("personal_notes", "readwrite");
+  let db;
+  const tx = db.transaction('personal_notes', 'readwrite');
   tx.onerror = (e) => alert(` Error! ${e.target.error}  `);
-  const pNotes = tx.objectStore("personal_notes");
+  const pNotes = tx.objectStore('personal_notes');
   pNotes.add(note);
   event.preventDefault();
 } /* addNote */
 
 function viewNote(event) {
-  const tx = db.transaction("personal_notes", "readonly");
-  const pNotes = tx.objectStore("personal_notes");
+  let db;
+  const tx = db.transaction('personal_notes', 'readonly');
+  const pNotes = tx.objectStore('personal_notes');
 
   const today = new Date();
-  const date =
-    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
   console.log(date);
   const request = pNotes.get(date);
 
@@ -62,19 +62,19 @@ function viewNote(event) {
   request.onsuccess = function (event) {
     // Do something with the request.result!
     console.log(request.result);
-    document.getElementById("noteList").innerHTML = request.result.text;
+    document.getElementById('noteList').innerHTML = request.result.text;
   };
 
   event.preventDefault();
 } /* viewNote */
 
 function updateNote(event) {
-  const tx = db.transaction("personal_notes", "readwrite");
-  const pNotes = tx.objectStore("personal_notes");
+  let db;
+  const tx = db.transaction('personal_notes', 'readwrite');
+  const pNotes = tx.objectStore('personal_notes');
 
   const today = new Date();
-  const date =
-    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  const date = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
   const request = pNotes.get(date);
 
   request.onerror = function (event) {
@@ -83,15 +83,15 @@ function updateNote(event) {
   };
   request.onsuccess = function (event) {
     // Do something with the request.result!
-    let data = event.target.result;
-    data.text = document.getElementById("noteList").innerHTML;
+    const data = event.target.result;
+    data.text = document.getElementById('noteList').innerHTML;
 
-    let requestUpdate = pNotes.put(data);
+    const requestUpdate = pNotes.put(data);
     requestUpdate.onerror = function (event) {
       console.log(error);
     };
     requestUpdate.onsuccess = function (event) {
-      console.log("updated");
+      console.log('updated');
     };
   };
 
