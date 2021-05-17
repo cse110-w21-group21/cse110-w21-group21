4c42a91ae6947eed1f12e6d15e9281cbf5573673
tpholston <tholston@ucsd.edu>
2021-05-16 19:34:33 -0700

    Calendar view w/ resizing
    there is functionality for localstorage event adding/deleting but we will need to switch this to indexedDB

---
+++ source/scripts/calendarView.js
@@ -0,0 +1,77 @@
+const eventCreation = document.getElementById('createEventForm');
+
+var calendar;
+function createCalendar(data) {
+  var calendarEl = document.getElementById('calendar');
+
+  calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    contentHeight: 'auto',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+
+    dateClick: function (date, jsEvent, view) {
+      if (this.view.type == 'dayGridMonth' || this.view.type == 'timeGridWeek') {
+        this.gotoDate(date.dateStr);
+        this.changeView('timeGridDay', date.dateStr);
+      }
+    },
+
+    eventClick: function (info) {
+      let date = info.event.start.toISOString().slice(0, 10);
+      if (this.view.type == 'dayGridMonth' || this.view.type == 'timeGridWeek') {
+        this.gotoDate(date);
+        this.changeView('timeGridDay', date);
+      } else {
+        let existing = localStorage.getItem('currentEvents');
+
+        existing = JSON.parse(existing);
+
+        for (let i = 0; i < existing.length; i++) {
+          if(existing[i].title == info.event.title) {
+            existing.splice(i,1);
+          }
+        }
+        localStorage.setItem('currentEvents', JSON.stringify(existing));
+
+        info.event.remove();
+      }
+    },
+    events: data
+  });
+
+  calendar.render();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  let existing = localStorage.getItem('currentEvents');
+  createCalendar(JSON.parse(existing));
+});
+
+/**eventCreation.addEventListener('submit', function () {
+  let inputTitle = document.getElementById('title').value;
+  let startDate = document.getElementById('start').value;
+  let endDate = document.getElementById('end').value;
+  let colorSelection = document.getElementById('colorSelect').value;
+  let existing = localStorage.getItem('currentEvents');
+
+  existing = JSON.parse(existing);
+  if(Array.isArray(existing)) {
+    existing.push({ title: inputTitle, start: startDate, end: endDate, color: colorSelection });
+  } else {
+    existing = { title: inputTitle, start: startDate, end: endDate, color: colorSelection }; 
+    existing = [ existing ];
+  }
+  localStorage.setItem('currentEvents', JSON.stringify(existing));
+
+  createCalendar(existing);
+});
+*/
+
+const menuToggle = document.querySelector('main');
+menuToggle.addEventListener('transitionend', () => {
+  calendar.updateSize();
+});
