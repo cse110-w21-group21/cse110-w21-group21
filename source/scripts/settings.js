456bfb30b2c007b5a653c2d82eed1eced4bd0b23
tpholston <tholston@ucsd.edu>
2021-05-18 18:54:14 -0700

    Settings Color Scheme
    color scheme first edition with only 3 different types of schemes.

---
+++ source/scripts/settings.js
@@ -0,0 +1,20 @@
+document.getElementById('one').addEventListener('click', () => {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+    document.body.classList.remove("invertedText");
+    localStorage.setItem("theme", "light");
+});
+
+document.getElementById('two').addEventListener('click', () => {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+    document.body.classList.remove("invertedText");
+    localStorage.setItem("theme", "dark");
+});
+
+document.getElementById('three').addEventListener('click', () => {
+    document.body.classList.remove("dark");
+    document.body.classList.remove("light");
+    document.body.classList.add("invertedText");
+    localStorage.setItem("theme", "invertedText");
+});
