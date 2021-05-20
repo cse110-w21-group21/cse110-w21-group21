d21f7a565757f6bfc89f953ac3bae735a91ccb90
tpholston <tholston@ucsd.edu>
2021-05-19 17:44:19 -0700

    restructure calendars
    some errors were being thrown. Should be good now

---
+++ source/scripts/calendar.js
@@ -0,0 +1,58 @@
+/*eslint-disable*/
+const eventCreation = document.getElementById('createEventForm');
+
+var calendar;
+function createCalendar(data) {
+  var calendarEl = document.getElementById('calendar');
+
+  calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    aspectRatio: 2,
+    headerToolbar: {
+      left: 'prev,next',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+
+    dateClick: function (date, jsEvent, view) {
+      if (this.view.type == 'dayGridMonth' || this.view.type == 'timeGridWeek') {
+        this.gotoDate(date.dateStr);
+        this.changeView('timeGridDay', date.dateStr);
+      }
+    },
+
+    eventClick: function (info) {
+      let date = info.event.start.toISOString().slice(0, 10);
+      if (this.view.type == 'dayGridMonth' || this.view.type == 'timeGridWeek') {
+        this.gotoDate(date);
+        this.changeView('timeGridDay', date);
+      } else {
+        let existing = localStorage.getItem('currentEvents');
+
+        existing = JSON.parse(existing);
+
+        for (let i = 0; i < existing.length; i++) {
+          if(existing[i].title == info.event.title) {
+            existing.splice(i,1);
+          }
+        }
+        localStorage.setItem('currentEvents', JSON.stringify(existing));
+
+        info.event.remove();
+      }
+    },
+    events: data
+  });
+
+  calendar.render();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  let existing = localStorage.getItem('currentEvents');
+  createCalendar(JSON.parse(existing));
+});
+
+const menuToggle = document.querySelector('main');
+menuToggle.addEventListener('transitionend', () => {
+  calendar.updateSize();
+});
--- source/scripts/calendarView.js
+++ source/scripts/calendarView.js
@@ -1,57 +1,3 @@
-/*eslint-disable*/
-const eventCreation = document.getElementById('createEventForm');
-
-var calendar;
-function createCalendar(data) {
-  var calendarEl = document.getElementById('calendar');
-
-  calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
-    aspectRatio: 2,
-    headerToolbar: {
-      left: 'prev,next',
-      center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay'
-    },
-
-    dateClick: function (date, jsEvent, view) {
-      if (this.view.type == 'dayGridMonth' || this.view.type == 'timeGridWeek') {
-        this.gotoDate(date.dateStr);
-        this.changeView('timeGridDay', date.dateStr);
-      }
-    },
-
-    eventClick: function (info) {
-      let date = info.event.start.toISOString().slice(0, 10);
-      if (this.view.type == 'dayGridMonth' || this.view.type == 'timeGridWeek') {
-        this.gotoDate(date);
-        this.changeView('timeGridDay', date);
-      } else {
-        let existing = localStorage.getItem('currentEvents');
-
-        existing = JSON.parse(existing);
-
-        for (let i = 0; i < existing.length; i++) {
-          if(existing[i].title == info.event.title) {
-            existing.splice(i,1);
-          }
-        }
-        localStorage.setItem('currentEvents', JSON.stringify(existing));
-
-        info.event.remove();
-      }
-    },
-    events: data
-  });
-
-  calendar.render();
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  let existing = localStorage.getItem('currentEvents');
-  createCalendar(JSON.parse(existing));
-});
-
 eventCreation.addEventListener('submit', function () {
   let inputTitle = document.getElementById('title').value;
   let startDate = document.getElementById('start').value;
@@ -69,10 +15,4 @@ eventCreation.addEventListener('submit', function () {
   localStorage.setItem('currentEvents', JSON.stringify(existing));
 
   createCalendar(existing);
-});
-
-
-const menuToggle = document.querySelector('main');
-menuToggle.addEventListener('transitionend', () => {
-  calendar.updateSize();
 });
