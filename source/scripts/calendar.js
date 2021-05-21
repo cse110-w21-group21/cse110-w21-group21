9eb372abb219c5e3e85bcd0f63a1e503812ed438
tpholston <tholston@ucsd.edu>
2021-05-21 12:45:15 -0700

    changed CalendarView calendar
    sending to Jerry

--- source/scripts/calendar.js
+++ source/scripts/calendar.js
@@ -9,35 +9,27 @@ function createCalendar(data) {
     headerToolbar: {
       left: 'prev,next',
       center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay'
     },
 
     dateClick: function (date,) {
-      if (this.view.type === 'dayGridMonth' || this.view.type === 'timeGridWeek') {
-        this.gotoDate(date.dateStr);
-        this.changeView('timeGridDay', date.dateStr);
-      }
+      localStorage.setItem('dateClicked', date.dateStr);
+      location.href = "./dailyLog.html";
     },
 
     eventClick: function (info) {
       let date = info.event.start.toISOString().slice(0, 10);
-      if (this.view.type === 'dayGridMonth' || this.view.type === 'timeGridWeek') {
-        this.gotoDate(date);
-        this.changeView('timeGridDay', date);
-      } else {
         let existing = localStorage.getItem('currentEvents');
 
         existing = JSON.parse(existing);
 
-        for (let i = 0; i < existing.length; i += 1) {
-          if(existing[i].title === info.event.title) {
-            existing.splice(i,1);
-          }
+      for (let i = 0; i < existing.length; i += 1) {
+        if(existing[i].title === info.event.title) {
+          existing.splice(i,1);
         }
-        localStorage.setItem('currentEvents', JSON.stringify(existing));
-
-        info.event.remove();
       }
+      localStorage.setItem('currentEvents', JSON.stringify(existing));
+
+      info.event.remove();
     },
     events: data
   });
