dde48b0e512436d173ceca0c8c7c37ad8d4cb0aa
JGuo24 <60760168+JGuo24@users.noreply.github.com>
2021-05-21 12:56:27 -0700

    Create dailyLogCalendar.js

---
+++ source/scripts/dailyLogCalender.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+var calendar;
+function createCalendar(data) {
+  var calendarEl = document.getElementById('calendar');
+
+  calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'timeGridDay',
+    aspectRatio: 2,
+    headerToolbar: {
+      center: 'title',
+    },
+
+    eventClick: function (info) {
+      let date = info.event.start.toISOString().slice(0, 10);
+        let existing = localStorage.getItem('currentEvents');
+
+        existing = JSON.parse(existing);
+
+      for (let i = 0; i < existing.length; i += 1) {
+        if(existing[i].title === info.event.title) {
+          existing.splice(i,1);
+        }
+      }
+      localStorage.setItem('currentEvents', JSON.stringify(existing));
+
+      info.event.remove();
+    },
+    events: data
+  });
+
+  calendar.render();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  let existing = localStorage.getItem('currentEvents');
+  createCalendar(JSON.parse(existing));
+});
+
+const menuToggle = document.querySelector('main');
+menuToggle.addEventListener('transitionend', () => {
+  calendar.updateSize();
+});
