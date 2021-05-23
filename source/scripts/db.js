7de2d47c54c6ee28432bc5700392c3cc35e6344c
Thomas Garry <32282600+thomas-gg@users.noreply.github.com>
2021-05-23 13:43:01 -0700

    added preliminary autosave

--- source/scripts/db.js
+++ source/scripts/db.js
@@ -3,26 +3,28 @@ let db;
 /**
  * Creates a database and/or upgrades the database
  */
-function createDB() {
-  const request = indexedDB.open('noteDB', 1);
+function createDB(fromWeekly) {
+  const request = indexedDB.open("noteDB", 1);
 
   // on upgrade needed --> if database doesn't exist
   request.onupgradeneeded = (e) => {
     db = e.target.result;
 
-    const objStore = db.createObjectStore('personal_notes', {
-      keyPath: 'date',
+    const objStore = db.createObjectStore("personal_notes", {
+      keyPath: "date",
     });
-    objStore.createIndex('date', 'date', { unique: false });
+    objStore.createIndex("date", "date", { unique: false });
+    addNoteDB(fromWeekly);
     console.log(
-      `upgrade is called database name: ${db.name} version : ${db.version}`,
+      `upgrade is called database name: ${db.name} version : ${db.version}`
     );
   };
   // on success
   request.onsuccess = (e) => {
     db = e.target.result;
+    addNoteDB(fromWeekly);
     console.log(
-      `success is called database name: ${db.name} version : ${db.version}`,
+      `success is called database name: ${db.name} version : ${db.version}`
     );
   };
   // on error
@@ -32,17 +34,16 @@ function createDB() {
 } /* createDB  */
 
 /**
- * Adds a note to the DB. Should be called when first creating a note
- * for the current day.
- *
- * @param {event} - event default click event
+ * Adds a note to the DB. Will be called from createDB to add a note.
+ * @param {fromWeekly} - Boolean - If it's from the weekly page
  */
-function addNoteDB(event) {
+function addNoteDB(fromWeekly) {
   // allRecords.innerHTML = "";
   const today = new Date();
-  const date = `${today.getFullYear()}-${today.getMonth() + 1
+  const date = `${today.getFullYear()}-${
+    today.getMonth() + 1
   }-${today.getDate()}`;
-  const noteString = document.getElementById('notelist').innerHTML;
+  const noteString = document.getElementById("notelist").innerHTML;
   const note = {
     time: Math.floor(Date.now() / 1000),
     date,
@@ -50,11 +51,18 @@ function addNoteDB(event) {
   };
   console.log(note);
 
-  const tx = db.transaction('personal_notes', 'readwrite');
+  const tx = db.transaction("personal_notes", "readwrite");
   tx.onerror = (e) => alert(` Error! ${e.target.error}  `);
-  const pNotes = tx.objectStore('personal_notes');
-  pNotes.add(note);
-  event.preventDefault();
+  const pNotes = tx.objectStore("personal_notes");
+  const request = pNotes.get(date);
+  request.onerror = function err(error) {
+    // duplicate note
+    pNotes.add(note);
+  };
+  request.onsuccess = function success() {
+    // note already exists, we need to view it
+    viewNote(fromWeekly);
+  };
 } /* addNote */
 
 /**
@@ -64,12 +72,13 @@ function addNoteDB(event) {
  * @param {fromWeekly} - Boolean - If it's from the weekly page
  * @param {event} - event default click event
  */
-function viewNote(event, fromWeekly) {
-  const tx = db.transaction('personal_notes', 'readonly');
-  const pNotes = tx.objectStore('personal_notes');
+function viewNote(fromWeekly) {
+  const tx = db.transaction("personal_notes", "readonly");
+  const pNotes = tx.objectStore("personal_notes");
 
   const today = new Date();
-  const date = `${today.getFullYear()}-${today.getMonth() + 1
+  const date = `${today.getFullYear()}-${
+    today.getMonth() + 1
   }-${today.getDate()}`;
   console.log(date);
   const request = pNotes.get(date);
@@ -81,32 +90,33 @@ function viewNote(event, fromWeekly) {
   request.onsuccess = function success() {
     // Do something with the request.result!
     console.log(request.result);
-    document.getElementById('notelist').innerHTML = request.result.text;
+    document.getElementById("notelist").innerHTML = request.result.text;
     if (fromWeekly) {
-      let unimportantNotes = document.querySelectorAll('bullet-note[data-important="false"]');
-      unimportantNotes.forEach((u) => { u.remove(); });
-      let noteTextboxes = document.querySelectorAll('.textbox');
+      let unimportantNotes = document.querySelectorAll(
+        'bullet-note[data-important="false"]'
+      );
+      unimportantNotes.forEach((u) => {
+        u.remove();
+      });
+      let noteTextboxes = document.querySelectorAll(".textbox");
       noteTextboxes.forEach((t) => {
         let t2 = t;
         t2.contentEditable = false;
       });
     }
   };
-
-  event.preventDefault();
 } /* viewNote */
 
 /**
- * Updates a note to the DB. Should be called after editing notes.
- *
- * @param {event} - event default click event
+ * Updates a note to the DB. Should be called after editing notes.s
  */
-function updateNote(event) {
-  const tx = db.transaction('personal_notes', 'readwrite');
-  const pNotes = tx.objectStore('personal_notes');
+function updateNote() {
+  const tx = db.transaction("personal_notes", "readwrite");
+  const pNotes = tx.objectStore("personal_notes");
 
   const today = new Date();
-  const date = `${today.getFullYear()}-${today.getMonth() + 1
+  const date = `${today.getFullYear()}-${
+    today.getMonth() + 1
   }-${today.getDate()}`;
   const request = pNotes.get(date);
 
@@ -117,20 +127,16 @@ function updateNote(event) {
   request.onsuccess = function success(e) {
     // Do something with the request.result!
     const data = e.target.result;
-    data.text = document.getElementById('notelist').innerHTML;
+    data.text = document.getElementById("notelist").innerHTML;
 
     const requestUpdate = pNotes.put(data);
     requestUpdate.onerror = function err(error) {
       console.log(error);
     };
     requestUpdate.onsuccess = function s() {
-      console.log('updated');
+      console.log("updated");
     };
   };
-
-  event.preventDefault();
 } /* updateNote */
 
-export {
-  createDB, addNoteDB, updateNote, viewNote,
-};
+export { createDB, addNoteDB, updateNote, viewNote };
