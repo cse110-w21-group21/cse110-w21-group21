16d7096b6cdbabeffcdff91971fbd1c695f56976
LexuanLiu123 <lel006@ucsd.edu>
2021-05-25 21:33:56 -0700

    sticky notes without js

---
+++ source/scripts/stickyNotes.js
@@ -0,0 +1,10 @@
+let redButton = document.getElementById('note-color-btn note-red');
+let orangeButton = document.getElementsByClassName('note-color-btn note-orange');
+let yellowButton = document.getElementsByClassName('note-color-btn note-yellow');
+let greenButton = document.getElementsByClassName('note-color-btn note-green');
+let blueButton = document.getElementsByClassName('note-color-btn note-blue');
+let purpleButton = document.getElementsByClassName('note-color-btn note-purple');
+
+redButton.addEventListener("click", function(){
+    console.log("hello");
+});
