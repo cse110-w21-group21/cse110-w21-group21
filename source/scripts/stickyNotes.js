22eb170d479666541eec096eac2b222dfd7927de
LexuanLiu123 <lel006@ucsd.edu>
2021-05-27 12:53:00 -0700

    added sticky notes feature

--- source/scripts/stickyNotes.js
+++ source/scripts/stickyNotes.js
@@ -1,10 +1,85 @@
-let redButton = document.getElementById('note-color-btn note-red');
-let orangeButton = document.getElementsByClassName('note-color-btn note-orange');
-let yellowButton = document.getElementsByClassName('note-color-btn note-yellow');
-let greenButton = document.getElementsByClassName('note-color-btn note-green');
-let blueButton = document.getElementsByClassName('note-color-btn note-blue');
-let purpleButton = document.getElementsByClassName('note-color-btn note-purple');
+let addNote = document.getElementById('note-add-btn');
+let mainNote = document.querySelectorAll('.note');
+let noteContainer = document.getElementById('sticky-note-container');
+let colorList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+let numberOfStickies = 0;
 
-redButton.addEventListener("click", function(){
-    console.log("hello");
+let redButton = document.querySelectorAll('#sticky-btn-red');
+let orangeButton = document.querySelectorAll('#sticky-btn-orange');
+let yellowButton = document.querySelectorAll('#sticky-btn-yellow');
+let greenButton = document.querySelectorAll('#sticky-btn-green');
+let blueButton = document.querySelectorAll('#sticky-btn-blue');
+let purpleButton = document.querySelectorAll('#sticky-btn-purple');
+let deleteButton = document.querySelectorAll('#sticky-btn-delete')
+
+function reloadVariables() {
+    mainNote = document.querySelectorAll('.note');
+    redButton = document.querySelectorAll('#sticky-btn-red');
+    orangeButton = document.querySelectorAll('#sticky-btn-orange');
+    yellowButton = document.querySelectorAll('#sticky-btn-yellow');
+    greenButton = document.querySelectorAll('#sticky-btn-green');
+    blueButton = document.querySelectorAll('#sticky-btn-blue');
+    purpleButton = document.querySelectorAll('#sticky-btn-purple');
+    deleteButton = document.querySelectorAll('#sticky-btn-delete');
+}
+
+function onClickColorBtn(num, color) {
+    mainNote[num].className = 'note note-' + color;
+}
+
+function addEventListenerColorBtn(i) {
+    redButton[i].addEventListener('click',function() {
+        onClickColorBtn(i, 'red');
+    });
+
+    orangeButton[i].addEventListener('click', function() {
+        onClickColorBtn(i, 'orange');
+    });
+
+    yellowButton[i].addEventListener('click', function() {
+        onClickColorBtn(i, 'yellow');
+    });
+
+    greenButton[i].addEventListener('click', function() {
+        onClickColorBtn(i, 'green');
+    });
+
+    blueButton[i].addEventListener('click', function() {
+        onClickColorBtn(i, 'blue');
+    });
+
+    purpleButton[i].addEventListener('click', function() {
+        onClickColorBtn(i, 'purple');
+    });
+
+    // TODO: fix the error that occurs when deleting the last note
+    deleteButton[i].addEventListener('click', function() {
+        if (numberOfStickies > 0) {
+            mainNote[i].remove();
+            numberOfStickies--;
+            reloadVariables();
+        }
+        else if (numberOfStickies == 0) {
+            console.log("cannot click on this, we have to have at least 1 note");
+        }
+    });
+}
+
+addEventListenerColorBtn(numberOfStickies);
+
+purpleButton[0].addEventListener('click', function() {
+    onClickColorBtn(0, 'purple');
+});
+
+addNote.addEventListener('click', function() {
+    let cloneMainNote = mainNote[numberOfStickies].cloneNode(true);
+    let randomColor = colorList[Math.floor(Math.random() * colorList.length)];
+    console.log(randomColor);
+    cloneMainNote.querySelectorAll('input')[0].value = '';
+    cloneMainNote.querySelectorAll('textarea')[0].value = '';
+    cloneMainNote.className = 'note note-' + randomColor; 
+    noteContainer.appendChild(cloneMainNote);
+    reloadVariables();
+    numberOfStickies++;
+    addEventListenerColorBtn(i);
 });
