63c9059993f7a0aa55a8bc91a87fc3af6c99122f
LexuanLiu123 <lel006@ucsd.edu>
2021-06-02 16:20:57 -0700

    autosave container [with bug]

--- source/scripts/autosave.js
+++ source/scripts/autosave.js
@@ -6,9 +6,16 @@ function getSticky() {
     return [title, textarea];
 }
 
+function getContainer() {
+    return document.getElementById('sticky-note-container');
+}
 
 function save() {
     var sticky = getSticky();
+    var container = getContainer();
+    if (container) {
+        localStorage.setItem("AUTOSAVE_container" + document.location, container.innerHTML);
+    }
     if (sticky) {
         localStorage.setItem("AUTOSAVE_title" + document.location, sticky[0].value);
         localStorage.setItem("AUTOSAVE_textarea" + document.location, sticky[1].value);
@@ -18,7 +25,12 @@ function save() {
 function restore() {
     var saved_title = localStorage.getItem("AUTOSAVE_title" + document.location);
     var saved_textarea = localStorage.getItem("AUTOSAVE_textarea" + document.location);
+    var saved_container = localStorage.getItem("AUTOSAVE_container" + document.location)
     var sticky = getSticky();
+    var container = getContainer();
+    if (container && saved_container) {
+        container.innerHTML = saved_container;
+    }
     if ((saved_title || saved_textarea) && sticky) {
         sticky[0].value = saved_title;
         sticky[1].value = saved_textarea;
