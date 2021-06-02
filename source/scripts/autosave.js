57bbed6c45b0c10df7efee546979213c73e7ca95
LexuanLiu123 <lel006@ucsd.edu>
2021-06-02 14:33:10 -0700

    autosaving first note every 3 seconds

---
+++ source/scripts/autosave.js
@@ -0,0 +1,42 @@
+var timer = null;
+
+function getSticky() {
+    var title = document.getElementById("mytitle");
+    var textarea = document.getElementById("mytextarea");
+    return [title, textarea];
+}
+
+
+function save() {
+    var sticky = getSticky();
+    if (sticky) {
+        localStorage.setItem("AUTOSAVE_title" + document.location, sticky[0].value);
+        localStorage.setItem("AUTOSAVE_textarea" + document.location, sticky[1].value);
+    }
+}
+
+function restore() {
+    var saved_title = localStorage.getItem("AUTOSAVE_title" + document.location);
+    var saved_textarea = localStorage.getItem("AUTOSAVE_textarea" + document.location);
+    var sticky = getSticky();
+    if ((saved_title || saved_textarea) && sticky) {
+        sticky[0].value = saved_title;
+        sticky[1].value = saved_textarea;
+    }
+}
+
+function start() {
+    var editor = getSticky();
+    if (editor[0].value.length || editor[1].value.length <= 0) {
+        restore();
+    }
+    if (timer) {
+        clearInterval(timer);
+        timer = null;
+    }
+
+    // autosaving every 5 seconds
+    timer = setInterval(save, 3000);
+}
+
+start();
