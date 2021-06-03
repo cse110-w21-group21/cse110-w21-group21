99e9a0f5dbfaf4af901c5c8824766f336dc3be20
LexuanLiu123 <lel006@ucsd.edu>
2021-06-03 15:16:59 -0700

    removed .env settings in config.js

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 module.exports = {
     launch: {
-        headless: process.env.HEADLESS !== 'false',
-        slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
+        headless: false,
+        slowMo: 0,
         devtools: true
     }
 }
