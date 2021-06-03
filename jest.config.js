e68f378dd77be1b3648991b4d56b39b9bde11637
LexuanLiu123 <lel006@ucsd.edu>
2021-06-03 14:35:49 -0700

    jest-puppeteer setup

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: "jest-puppeteer",
+    globals: {
+      URL: "http://127.0.0.1:5500/source/index.html"
+    },
+    testMatch: [
+      "**/test/**/*.test.js"
+    ],
+    verbose: true
+}
---
+++ source/test/frontent.test.js
@@ -0,0 +1,19 @@
+const timeout = process.env.SLOWMO ? 30000 : 10000;
+
+beforeAll(async () => {
+    await page.goto(URL, {waitUntil: 'domcontentloaded'});
+});
+
+describe('Test header and title of the page', () => {
+
+    test('Title of the page', async () => {
+        const title = await page.title();
+        expect(title).toBe('NinePlusTen');
+    }, timeout);
+
+    test('Stickies of the page', async () => {
+        await page.click("#daily-log");
+        const url = await page.url();
+        expect(url).toBe("http://127.0.0.1:5500/source/dailyLog.html");
+    }, timeout);
+});
