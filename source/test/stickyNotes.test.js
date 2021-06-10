f59d92eff0c0a7bf41adfb3fa53324ea9571caaf
LexuanLiu123 <lel006@ucsd.edu>
2021-06-09 21:27:00 -0700

    stickyNotes basic test (w/o localstorage)

--- source/test/stickyNotes.test.js
+++ source/test/stickyNotes.test.js
@@ -32,4 +32,54 @@ describe('Test Sticky Notes Functionalities', () => {
         },
         timeout,
     );
+    test(
+        'Click on delete note button alert message',
+        async() => {
+            await page.waitForSelector('#sticky-btn-delete');
+            await page.click('#sticky-btn-delete');
+            page.on('dialog', async (dialog) => {
+                expect(dialog.message()).toBe('cannot remove the last note!');
+                console.log(dialog.message());
+                await dialog.dismiss();
+                await browser.close();
+            });
+        },
+        timeout,
+    );
+    test(
+        'Click on orange button',
+        async () => {
+            await page.click('#sticky-btn-orange');
+            try {
+                await page.$eval('.note.note-orange', (e) => e.innerHTML);
+                console.log('Click on orange button success.');
+            } catch (e){
+                console.error('Click on orange button failed.');
+            }
+        },
+        timeout,
+    );
+    test(
+        'Click on add note button',
+        async() => {
+            await page.click('#note-add-btn');
+            await page.click('#note-add-btn');
+            const numStickies = await page.$$eval(".note", (stickies) => {
+                return stickies.length;
+              });
+            expect(numStickies).toBe(3);
+        },
+        timeout,
+    );
+    test(
+        'Go to Settings',
+        async () => {
+          await page.waitForSelector('a[title="Settings"]');
+          await page.waitForTimeout(500);
+          await page.click('a[title="Settings"]');
+          const url = await page.url();
+          expect(url).toBe('http://127.0.0.1:5500/source/settings.html');
+        },
+        timeout,
+    );
 });
